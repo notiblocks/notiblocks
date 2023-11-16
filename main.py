0111faf884e2becd9db8ea49973935832ec3d3da
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2023-11-16 10:25:14 +0200

    some more examples

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+import random
 from notiblocks import NBConfig, NBHandler
 
 # METHODS
@@ -8,7 +10,7 @@ def main():
         success_sign_color="blue",
         time_sign_color="red",
         success_sign="SUCCESS",
-        success_bracket_color="can"
+        success_bracket_color="can" # Wrong, should throw an exception
     )
 
     nb_handler = NBHandler(nb_conf)
@@ -17,6 +19,15 @@ def main():
     print(nb_handler.fail("And this is a failed message"))
     print(nb_handler.warn("But this is a warning message"))
     print(nb_handler.log("This is a logged message."))
+
+    id = 0
+
+    print("== LOGS ==")
+    while True:
+        delay = random.uniform(1, 3)
+        print(nb_handler.log(f"New registration -- USER-id: {id}"))
+        id += 1
+        time.sleep(delay)
 
 
 # MAIN
