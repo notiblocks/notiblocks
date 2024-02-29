98a1f1b72b6bddc3492e29dc5d112bdfb27c6117
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2024-02-29 08:35:43 +0200

    Inline formatting done to a certain point, new configuration ideas present

--- notiblocks/main/inline_formatting/il_config.py
+++ notiblocks/main/inline_formatting/il_config.py
@@ -1,14 +1,26 @@
-# Inline config class
+
 
 class ILConfig:
-    
-    def __init__ (self, color: str):
+    """
+    ILConfig is a configuration class for the in-line formatting tool "ILFormatter".
+    For the current version it supports only color formatting.
+    """
+    def __init__ (self, color: str, text: str):
         self._color = color
+        self._text = text
 
     @property
     def color(self) -> str:
         return self._color
 
+    @property
+    def text(self) -> str:
+        return self._text
+
     @color.setter
     def color(self, value: str):
-        self.color = value.lower().strip()
+        self._color = value.lower().strip()
+
+    @text.setter
+    def test(self, value: str):
+        self._text = value.lower().strip()
--- notiblocks/main/inline_formatting/il_formatter.py
+++ notiblocks/main/inline_formatting/il_formatter.py
@@ -1,12 +1,40 @@
 # Inline formatter class, wrapper of the il_config
+from il_config import ILConfig
 
-class ILFormatter():
-    
-    def __init__ (self, configuration: ILConfig):
-        self.configuration = configuration
+from ..ansi import ANSI
+from .enums.colors.fgcolors import FGColors
+
+DEFAULT_COLOR = "none"
+
+class ILFormatter:
+    """
+    ILFormatter is a class, that let's you format your message inline.
+    It's job is to work like a factory - you provide a list of ILConfigs, and it returns
+    a string, that represents them.
+
+    To use it, just call the .format() method and provide the color names and the text.
+    The way that you order them one next to another is how your code would be colored.
+
+    You could tell it, that you want to format the text like $this$
+    """
+
+    def __init__ ():
+        pass
+
+    def __convert_formats(inline_configurations: list) -> str:
+        pass
+        
 
     def format(self, message: str, args: list) -> str:
-        # 1. Split by $ to an array
-        # 2. replace the format of the message and the current format
-        # 3. Find a way to reapply the format again, so it does not get overwritten :?
-        pass
+
+        message_args = message.split('$')
+        inline_configurations = [] # holder for the configuration classes
+
+        # Define the configurations
+        for i in range(0, len(message_args)):
+            if i % 2 != 0:
+                inline_configurations[i] = ILConfig(message_args[i], args[i].color.lower())
+            else:
+                inline_configurations[i] = ILConfig(message_args[i], DEFAULT_COLOR)
+
+        return __convert_formats(inline_configurations)
