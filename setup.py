fad9200c78ff589c1c27996855ee26ec9975b4be
D3Y4N <103333790+Deyan2306@users.noreply.github.com>
2024-03-01 21:18:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
+import subprocess
+import os
 
-VERSION = '0.0.3'
+VERSION="0.0.3"
 DESCRIPTION = 'Logging library for the impatient'
-LONG_DESCRIPTION = 'Easy way to make your terminal logs more colorful.'
+long_description=""
+
+with open('README.md', 'r', encoding='utf-8') as fd:
+    long_description=fd.read()
 
 # Setting up
 setup(
@@ -12,7 +17,7 @@ setup(
     author_email="dvs_sec@proton.me",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     packages=find_packages(),
     install_requires=[],
     keywords=['python', 'logging', 'warning', 'terminal', 'ansi', 'color'],
