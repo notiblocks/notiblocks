f657276b7b9a4a02e6ad8ba7f9bf9b098447e532
Deyan2306 <deyan.sirakov2006@abv.bg>
2024-03-04 14:46:10 +0200

    added version to the init

--- notiblocks/__init__.py
+++ notiblocks/__init__.py
@@ -2,3 +2,5 @@ from notiblocks.main.nb_config import NBConfig
 from notiblocks.main.nb_handler import NBHandler
 from notiblocks.main.inline_formatting.il_config import ILConfig
 from notiblocks.main.inline_formatting.il_formatter import ILFormatter
+
+__version__ = "0.0.4"
