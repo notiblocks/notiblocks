70fff3d0bd3774fc5439e848f3bed4495a02f9d4
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2024-03-05 09:05:44 +0200

    Errors are moved to an _error module, so now there is no need for separate classes for each error

---
+++ notiblocks/main/_error.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+from ._ansi import ANSI
+from ._constants import *
+
+class NBError(ABC):
+
+    def __init__(self, message: str):
+        slef._message = message
+
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+
+class InvalidFormatError(NBError, RuntimeError):
+    def __str__(self) -> str:
+        return f"{ANSI.background(BG_RED)}{ANSI.color_text(FG_YELLOW)}[{ANSI.color_text(FG_WHITE)}EXCEPTION{ANSI.color_text(FG_YELLOW)}]{ANSI.color_text(FG_WHITE)}{self._message}{RESET_STYLE}"
+
+class InvalidOperationTypeError(NBError, RuntimeError):
+    def __str__(self) -> str:
+        return f"{ANSI.background(BG_RED)}{ANSI.color_text(FG_YELLOW)}[{ANSI.color_text(FG_WHITE)}EXCEPTION{ANSI.color_text(FG_YELLOW)}]{ANSI.color_text(FG_WHITE)}{self._message}{RESET_STYLE}"
