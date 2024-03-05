28e4255f612e17d91ce2d6636d740ce3ca283058
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2024-03-05 08:13:54 +0200

    Simple logging done

--- notiblocks/main/_logger.py
+++ notiblocks/main/_logger.py
@@ -5,3 +5,20 @@ class Logger:
     """
     def __init__(self):
         pass
+
+        # TODO: add the four types of logging here
+
+
+    def log(message: str, sign: str):
+        if sign.trim() == '':
+            sign = '[+]'
+
+        if message.trim == '':
+            pass # TODO: Throw and handle an exception
+
+        print(sign, message) 
+
+
+# Got an idea. Expose only the logger, so you could
+# internally configure it, but when it comes to logging,
+# use only this utility class.
