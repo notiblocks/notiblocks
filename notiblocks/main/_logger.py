9dfaca358792542be4763a6f88ef63bb4224ed81
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2024-03-05 08:05:36 +0200

    Logger utility added

--- notiblocks/main/_logger.py
+++ notiblocks/main/_logger.py
@@ -1,1 +1,7 @@
 # Logger utility for notiblocks
+class Logger:
+    """
+    Logger utility for notiblocks, it provides a simple inline logging functionality.
+    """
+    def __init__(self):
+        pass
